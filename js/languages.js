63e70423cd9cea85d790f316c4c850770a1feb85
javipzv <90279135+javipzv@users.noreply.github.com>
2024-04-17 17:22:10 +0200

    cleaned translation code

--- js/languages.js
+++ js/languages.js
@@ -1,96 +1,17 @@
 function changeToSpanish() {
-    document.getElementById("text-aboutme").innerHTML = "Sobre mí";
-    document.getElementById("text-projects").innerHTML = "Proyectos";
-    document.getElementById("text-skills").innerHTML = "Habilidades";
-    document.getElementById("text-contact").innerHTML = "Contacto";
-    document.getElementById("text-aboutme2").innerHTML = "Sobre mí";
-    document.getElementById("navbarSupportedContent").style.marginRight = "105px";
-    document.getElementById("text-description").innerHTML = "Actualmente estudio Ciencia de Datos e Inteligencia Artificial \
-    en la Universidad Politécnica de Madrid. Me gusta aprender y \
-    explorar el mundo de las tecnologías en general, ampliando mis conocimientos \
-    y habilidades. A nivel personal, me considero una persona que disfruta de \
-    la oportunidad de aplicar mis habilidades para impactar positivamente en la gente que me rodea.";
-    document.getElementById("text-CV").innerHTML = "Descargar CV";
-    document.getElementById("text-projects2").innerHTML = "Mis proyectos";
-    document.getElementById("text-title-clustering").innerHTML = "Análisis de un dataset de frutas y uso de algoritmos de clustering";
-    document.getElementById("text-description-clustering").innerHTML = "Análisis de un dataset de frutas que abarca la exploración \
-    de los datos, su preproceso, aplicación de métodos de clustering y la discusión de los resultados."
-    document.getElementById("text-title-kmeans").innerHTML = "K-Means desde cero";
-    document.getElementById("text-description-kmeans").innerHTML = "Implementación de K-Means desde cero en Python, \
-    usando NumPy. Incluye tests para distintos datasets.";
-    document.getElementById("text-title-digits").innerHTML = "Clasificador de dígitos (NN y CNN)";
-    document.getElementById("text-description-digits").innerHTML = "Este proyecto tiene el objetivo de desarrollar un clasificador de dígitos \
-    permitiendo así el reconocimento";
-    document.getElementById("text-title-hotel").innerHTML = "Predicción de reservas en hoteles";
-    document.getElementById("text-description-hotel").innerHTML = "Este trabajo cubre el preproceso y el análisis de un dataset de reservas en hoteles, \
-    haciendo un análisis exploratorio de los parámetros de la NN";
-    document.getElementById("text-title-stitching").innerHTML = "Homografías y técnicas de panorámicas de imágenes";
-    document.getElementById("text-description-stitching").innerHTML = "Unión de imágenes usando homografías, aplicando distintos métodos de detección de keypoints, \
-    técnicas de matching, RANSAC y proyecciones.";
-    document.getElementById("text-title-airlines").innerHTML = "Predicción de satisfacción de pasajeros";
-    document.getElementById("text-description-airlines").innerHTML = "Análisis de un conjunto de datos de encuestas sobre la satisfacción de los pasajeros de aerolíneas, \
-    utilizando algoritmos como Árboles de Decisión o SVM.";
-    document.getElementById("text-title-clustering2").innerHTML = "Análisis de un dataset de frutas y uso de algoritmos de clustering";
-    document.getElementById("text-description-clustering2").innerHTML = "Análisis de un dataset de frutas que abarca la exploración \
-    de los datos, su preproceso, aplicación de métodos de clustering y la discusión de los resultados."
-    document.getElementById("text-title-kmeans2").innerHTML = "Implementación de K-Means desde cero";
-    document.getElementById("text-description-kmeans2").innerHTML = "Implementación de K-Means desde cero en Python, \
-    usando NumPy. Incluye tests para distintos datasets.";
-    document.getElementById("text-title-stitching2").innerHTML = "Homografías y técnicas de panorámicas de imágenes";
-    document.getElementById("text-description-stitching2").innerHTML = "Unión de imágenes usando homografías, aplicando distintos métodos de detección de keypoints, \
-    técnicas de matching, RANSAC y proyecciones.";
-    document.getElementById("text-title-airlines2").innerHTML = "Predicción de satisfacción de pasajeros";
-    document.getElementById("text-description-airlines2").innerHTML = "Análisis de un conjunto de datos de encuestas sobre la satisfacción de los pasajeros de aerolíneas, \
-    utilizando algoritmos como Árboles de Decisión o SVM.";
-    document.getElementById("text-skills2").innerHTML = "Mis habilidades";
-    document.getElementById("text-contact2").innerHTML = "Contacto";
-    document.getElementById("text-form-subject").innerHTML = "Asunto";
-    document.getElementById("text-form-message").innerHTML = "Mensaje";
-    document.getElementById("text-form-send").innerHTML = "Enviar";
-    document.getElementById("text-contact3").innerHTML = "Contacto";
+    document.querySelectorAll(".english").forEach(element => {
+        element.style.display = "none";
+    });
+    document.querySelectorAll(".spanish").forEach(element => {
+        element.style.display = "inline-block";
+    });
 }
 
 function changeToEnglish() {
-    document.getElementById("text-aboutme").innerHTML = "About me";
-    document.getElementById("text-projects").innerHTML = "Projects";
-    document.getElementById("text-skills").innerHTML = "Skills";
-    document.getElementById("text-contact").innerHTML = "Contact";
-    document.getElementById("text-aboutme2").innerHTML = "About me";
-    document.getElementById("navbarSupportedContent").style.marginRight = "180px";
-    document.getElementById("text-description").innerHTML = "Currently, I am studying Data Science and Artificial Intelligence \
-    at the Universidad Politécnica de Madrid. I enjoy learning and exploring the world of technologies in general, expanding my \
-    knowledge and skills. On a personal level, I consider myself a person who enjoys the opportunity to apply my skills to positively \
-    impact the people around me.";
-    document.getElementById("text-CV").innerHTML = "Download CV";
-    document.getElementById("text-projects2").innerHTML = "My projects";
-    document.getElementById("text-title-clustering").innerHTML = "Fruit Dataset Analysis And Clustering Techniques";
-    document.getElementById("text-description-clustering").innerHTML = "Analysis of a fruit dataset. It covers data exploration, \
-    preprocessing techniques, the application of various clustering methods, and discussion of the results.";
-    document.getElementById("text-title-kmeans").innerHTML = "K-Means Implementation From Scratch";
-    document.getElementById("text-description-kmeans").innerHTML = "Python implementation of the K-Means clustering algorithm from \
-    scratch (using NumPy). It includes tests on different datasets";
-    document.getElementById("text-title-stitching").innerHTML = "Homographies and Image Stitching Techniques";
-    document.getElementById("text-description-stitching").innerHTML = "Image stitching using homographies, applying different methods \
-    of keypoint detection, matching techniques, RANSAC and projections.";
-    document.getElementById("text-title-airlines").innerHTML = "Airline Passenger Satisfaction Prediction";
-    document.getElementById("text-description-airlines").innerHTML = "Analysis of a dataset of some polls about airline passengers satisfaction \
-    and other data using algorithms such as Decision Trees or SVM.";
-    document.getElementById("text-title-clustering2").innerHTML = "Fruit Dataset Analysis And Clustering Techniques";
-    document.getElementById("text-description-clustering2").innerHTML = "Analysis of a fruit dataset. It covers data exploration, \
-    preprocessing techniques, the application of various clustering methods, and discussion of the results."
-    document.getElementById("text-title-kmeans2").innerHTML = "K-Means Implementation From Scratch";
-    document.getElementById("text-description-kmeans2").innerHTML = "Python implementation of the K-Means clustering algorithm from \
-    scratch (using NumPy). It includes tests on different datasets";
-    document.getElementById("text-title-stitching2").innerHTML = "Homographies and Image Stitching Techniques";
-    document.getElementById("text-description-stitching2").innerHTML = "Image stitching using homographies, applying different methods \
-    of keypoint detection, matching techniques, RANSAC and projections.";
-    document.getElementById("text-title-airlines2").innerHTML = "Airline Passenger Satisfaction Prediction";
-    document.getElementById("text-description-airlines2").innerHTML = "Analysis of a dataset of some polls about airline passengers satisfaction \
-    and other data using algorithms such as Decision Trees or SVM.";
-    document.getElementById("text-skills2").innerHTML = "My skills";
-    document.getElementById("text-contact2").innerHTML = "Contact";
-    document.getElementById("text-form-subject").innerHTML = "Subject";
-    document.getElementById("text-form-message").innerHTML = "Message";
-    document.getElementById("text-form-send").innerHTML = "Send";
-    document.getElementById("text-contact3").innerHTML = "Contact";
+    document.querySelectorAll(".english").forEach(element => {
+        element.style.display = "inline-block";
+    });
+    document.querySelectorAll(".spanish").forEach(element => {
+        element.style.display = "none";
+    });
 }
